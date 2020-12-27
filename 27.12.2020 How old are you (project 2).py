c2064993cb3fa22f19042dd18179327a24bc5770
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2020-12-27 16:34:11 +0300

    update

--- 27.12.2020 How old are you (project 2).py
+++ 27.12.2020 How old are you (project 2).py
@@ -1,6 +1,10 @@
 print("Hello user")
 name = input("what is your name?      ")
-print("Hello", name)
+print("Hello", name,"in your name",len(name),"letters")
 year = int(input("When did you born?   "))
 print(year,"Your age")
-print(2020 - year)
+if len(str(year)) == 4:
+    print(len(str(year)))
+    print(2020 - year)
+else:
+    print("You make mistake")
