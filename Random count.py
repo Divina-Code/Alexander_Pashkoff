2f03b2b59c5de7b56031b61110bfc1dd43fa0fc3
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2020-12-27 19:38:28 +0300

    Random project, first homewor

---
+++ Random count.py
@@ -0,0 +1,20 @@
+import random
+random_count = random.randrange(100)
+decide = False
+a = 0
+bad = -1
+while decide != True:
+    my_count = int(input("Please,enter the estimated number\n          "))
+    a = a + 1
+    if my_count == random_count:
+        print("Wow, this is the correct answer\n")
+        decide = True
+    elif my_count == bad:
+        print("It happened before\n")
+    elif my_count >= random_count:
+        print("Take less\n")
+        bad = int(my_count)
+    elif my_count <= random_count:
+        print("Take more\n")
+        bad = int(my_count)
+print("You guessed this number,\n You guessed right by", a,"trying")
