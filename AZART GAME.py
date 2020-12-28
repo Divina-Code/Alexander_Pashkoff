a1ea9ca5c13a1ad95a75f8a29e3c830d6260cbe6
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2020-12-28 19:26:26 +0300

    /

--- AZART GAME.py
+++ AZART GAME.py
@@ -1,7 +1,7 @@
 import random
-player_count_1 = 0
-player_count_2 = 0
-player_count_3 = 0
+player_count_1 = int(0)
+player_count_2 = int(0)
+player_count_3 = int(0)
 win1 = False
 win2 = False
 win3 = False
@@ -18,8 +18,8 @@ above1 = False
 above2 = False
 above3 = False
 print("Если хотите взять карту пишите да, если не хотите пишите нет")
-while deaf1 or above1 or win1 != True or deaf2 or above2 or win2 != True or deaf3 or above3 or win3 != True:
-    if  above1 == False and deaf1 == False:
+while (deaf1 or above1 or win1 != True) and (deaf2 or above2 or win2 != True) and (deaf3 or above3 or win3 != True):
+    if above1 or deaf1 == False:
         print("ХОД ", name1)
         want1 = input("Берёте карту? ")
         if want1 == "да":
@@ -35,7 +35,7 @@ while deaf1 or above1 or win1 != True or deaf2 or above2 or win2 != True or deaf
         else:
             print(name1,"вышел из игры со счётом",player_count_1)
             above1 = True
-    if  above2 == False and deaf2 == False:
+    if  above2 or deaf2 == False:
         print("ХОД ", name2)
         want2 = input("Берёте карту? ")
         if want2 == "да":
@@ -51,7 +51,7 @@ while deaf1 or above1 or win1 != True or deaf2 or above2 or win2 != True or deaf
         else:
             print(name2,"вышел из игры со счётом",player_count_2)
             above2 = True
-    if  above3 == False and deaf3 == False:
+    if  above3 or deaf3 == False:
         print("ХОД ", name3)
         want3 = input("Берёте карту? ")
         if want3 == "да":
@@ -59,7 +59,7 @@ while deaf1 or above1 or win1 != True or deaf2 or above2 or win2 != True or deaf
             print("Ваше количество очков", player_count_3)
             if player_count_3 > 21:
                 print("Ваши очки превышают 21",name3,"проиграл")
-                deaf2 = True
+                deaf3 = True
             elif player_count_3 == 21:
                 print("Читер или нет, но ты  выиграл", name3, "победил")
                 win3 = True
@@ -67,7 +67,28 @@ while deaf1 or above1 or win1 != True or deaf2 or above2 or win2 != True or deaf
         else:
             print(name3,"вышел из игры со счётом",player_count_3)
             above3 = True
-if player_count_1 :
+
+print("#"*25)
+print("#"*25)
+print("#"*25)
+if win1 == True:
+    print(name1,"ВЫИГРАЛ")
+elif deaf1 == True:
+    print(name1,"ПРОИГРАЛ")
+else:
+    print(name1,"ЗАРАБОТАЛ",player_count_1,"БАЛЛОВ")
+if win2 == True:
+    print(name2,"ВЫИГРАЛ")
+elif deaf2 == True:
+    print(name2,"ПРОИГРАЛ")
+else:
+    print(name2,"ЗАРАБОТАЛ",player_count_2,"БАЛЛОВ")
+if win3 == True:
+    print(name3,"ВЫИГРАЛ")
+elif deaf3 == True:
+    print(name3,"ПРОИГРАЛ")
+else:
+    print(name3,"ЗАРАБОТАЛ",player_count_3,"БАЛЛОВ")
 
 print("Конец")
 
