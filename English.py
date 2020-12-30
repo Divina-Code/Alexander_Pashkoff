4df9dcbbcc3c59d8ca68e5c9b44fe84b48f5ad84
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2020-12-30 17:06:23 +0300

    ...

---
+++ English.py
@@ -0,0 +1,17 @@
+d = {"t" : "b"}
+s = input("Нажмите L чтобы добавить слово, Нажмите R чтобы открыть список, Нажмите Q чтобы начть играть    ")
+if s == "L":
+    a = [input("Скажите слово и перевод   ").split()]
+    if len(a) == 2
+        name = a[0]
+        translate = a[1]
+            if d.get(name) == None:
+                d[name] = translate
+            else:
+                print("Такое слово уже есть ")
+    else:
+        print("Попробуй ещё раз ")
+
+
+
+
