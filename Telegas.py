ce34f1e409a310b3e333707eb780f5bd6ee40b00
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2021-01-04 18:00:01 +0300

    Update Telegas.py

--- Telegas.py
+++ Telegas.py
@@ -1,28 +1,33 @@
 import telebot
 import random
 count = random.randint(0, 100)
+a = 0
 bot = telebot.TeleBot('1488309812:AAEQfBGPxx6NkB5cSEud4WpEJz2GyPX7H_4')
 @bot.message_handler(content_types = ['text'])
 def otvet_na_text(message):
     global count
+    global a
 #    bot.send_message(message.chat.id, count)
     message.text.lower().split()
     if str(message.text) == "return":
         bot.send_message(message.chat.id, "OKAY")
         count = random.randint(0, 100)
+        a = 1
 
     elif int(message.text) == count:
         bot.send_message(message.chat.id, "It is")
         count = random.randint(0, 100)
         bot.send_message(message.chat.id, "#" * 30)
         bot.send_message(message.chat.id, "YOU GUESS THE NUMBER")
-
+        bot.send_message(message.chat.id, a, "Attempts spent")
+        a = 1
     elif int(message.text) > count:
-
         bot.send_message(message.chat.id, "Take less")
-    elif int(message.text) < count:
+        a = a + 1
 
+    elif int(message.text) < count:
         bot.send_message(message.chat.id,"Take more")
+        a = a + 1
 
 
 bot.polling()
