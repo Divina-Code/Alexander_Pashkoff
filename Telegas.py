d7017d0b5bc2698af1ceebef868d1fe9ae2370ba
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2021-01-04 17:38:08 +0300

    update

--- Telegas.py
+++ Telegas.py
@@ -5,21 +5,24 @@ bot = telebot.TeleBot('1488309812:AAEQfBGPxx6NkB5cSEud4WpEJz2GyPX7H_4')
 @bot.message_handler(content_types = ['text'])
 def otvet_na_text(message):
     global count
-    bot.send_message(message.chat.id, count)
+#    bot.send_message(message.chat.id, count)
     message.text.lower().split()
-    if int(message.text) == count:
+    if str(message.text) == "return":
+        bot.send_message(message.chat.id, "OKAY")
+        count = random.randint(0, 100)
+
+    elif int(message.text) == count:
         bot.send_message(message.chat.id, "It is")
         count = random.randint(0, 100)
-    elif message.text.lower() == "return":
-        count = random.randint(0, 100)
-        bot.send_message(message.chat.id, "Take less")
+        bot.send_message(message.chat.id, "#" * 30)
+        bot.send_message(message.chat.id, "YOU GUESS THE NUMBER")
+
     elif int(message.text) > count:
+
         bot.send_message(message.chat.id, "Take less")
     elif int(message.text) < count:
-        bot.send_message(message.chat.id,"Take more")
-    else:
-        bot.send_message(message.chat.id,"It is not")
 
+        bot.send_message(message.chat.id,"Take more")
 
 
 bot.polling()
