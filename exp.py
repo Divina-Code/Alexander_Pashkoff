dbbef2610d0dc1bd1e4ccc5d2f379fe788b82b99
AlexanderPashkoff <76618068+AlexanderPashkoff@users.noreply.github.com>
2021-01-05 18:48:19 +0300

    update

--- exp.py
+++ exp.py
@@ -1,8 +1,11 @@
-import pygame
+import pygame,random
 radious = 15
 pygame.init()
 gamedisplay = pygame.display.set_mode((800, 600))
 gamedisplay.fill((255, 255, 255))
+acolor = 0
+scolor = 0
+dcolor = 0
 game = True
 while game:
     event = pygame.event.get()
@@ -14,8 +17,12 @@ while game:
                 radious += 1
             if e.button == 5:
                 radious -= 1
+            if e.button == pygame.BUTTON_RIGHT:
+                acolor = random.randint(0, 255)
+                scolor = random.randint(0, 255)
+                dcolor = random.randint(0, 255)
     if pygame.mouse.get_pressed()[0]:
         mousepose = pygame.mouse.get_pos()
-        pygame.draw.circle(gamedisplay, (255, 0, 0), mousepose, radious)
+        pygame.draw.circle(gamedisplay, (acolor, scolor, dcolor), mousepose, radious)
     pygame.display.update()
 pygame.quit()
